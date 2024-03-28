d2a0fda06480a46a5ca75d17a8c9641f18781e4c
omarlahkim <omarlahkim98@gmail.com>
2024-03-28 22:06:57 +0000

    fixed android vector-icons and facebook SDK

---
+++ __tests__/unittesting/auth.ts
@@ -0,0 +1,19 @@
+//Test Email Login
+
+//Test Oauth Login
+
+// Test Email Register
+
+// Test Oauth Register
+
+// Test Logout
+
+// Test Forgot Password
+
+// Test Reset Password
+
+// Test Update Profile
+
+// Test Update Password
+
+// Test Delete Account
